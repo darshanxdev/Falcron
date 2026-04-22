50b337d4cd2f732e0562525365daa3f5dda92bb0
AeroX <aeroxdevs@gmail.com>
2026-04-22 17:47:02 +0530

    Initial commit: add src/utils/index.js

---
+++ src/utils/index.js
@@ -0,0 +1,23 @@
+// Falcron | AeroX Development
+// Author: itsfizys
+export * from './logger.js';
+export * from './permissionHandler.js';
+export * from './common.js';
+export * from './disableComponents.js';
+export * from './messageCounter.js';
+export * from './inviteCounter.js';
+export * from './leaderboardBuilder.js';
+export * from './invitedListBuilder.js';
+export * from './serverlistBuilder.js';
+export * from './resolveInviteVariables.js';
+export * from './resolveGreetVariables.js';
+
+/**
+ * Project: Falcron
+ * Author: itsfizys (Aegis)
+ * Organization: AeroX Development
+ * GitHub: https://github.com/AeroXDevs
+ * License: Custom
+ *
+ * © 2026 AeroX Development. All rights reserved.
+ */
