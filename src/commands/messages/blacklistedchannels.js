7b35e56ac524eae37fbf6677f8dbb208ec4d6126
AeroX <aeroxdevs@gmail.com>
2026-04-22 17:46:18 +0530

    Initial commit: add src/commands/messages/blacklistedchannels.js

---
+++ src/commands/messages/blacklistedchannels.js
@@ -0,0 +1,67 @@
+// Falcron | AeroX Development
+// Author: itsfizys
+import { Command } from '#command';
+import {
+        MessageFlags,
+        ContainerBuilder,
+        TextDisplayBuilder,
+        SeparatorBuilder,
+        SeparatorSpacingSize,
+} from 'discord.js';
+import { db } from '#dbManager';
+
+class BlacklistedChannelsCommand extends Command {
+        constructor() {
+                super({
+                        name: 'blacklistedchannels',
+                        description: 'Lists all channels blacklisted from message counting',
+                        aliases: ['blchannels'],
+                        cooldown: 5,
+                        enabledSlash: true,
+                        slashData: {
+                                name: 'blacklistedchannels',
+                                description: 'Lists all channels blacklisted from message counting',
+                        },
+                });
+        }
+
+        async execute({ ctx }) {
+                const list = await db.guild?.getMessageBlacklistedChannels(ctx.guild.id) ?? [];
+
+                const container = new ContainerBuilder();
+                container.setAccentColor(0xffffff);
+
+                container.addTextDisplayComponents(
+                        new TextDisplayBuilder().setContent(`**Blacklisted channels**`),
+                );
+
+                container.addSeparatorComponents(
+                        new SeparatorBuilder().setDivider(true).setSpacing(SeparatorSpacingSize.Small),
+                );
+
+                container.addTextDisplayComponents(
+                        new TextDisplayBuilder().setContent(
+                                list.length > 0
+                                        ? list.map((id) => `<#${id}>`).join(' ')
+                                        : 'No channels are blacklisted',
+                        ),
+                );
+
+                await ctx.reply({
+                        components: [container],
+                        flags: MessageFlags.IsComponentsV2,
+                });
+        }
+}
+
+export default new BlacklistedChannelsCommand();
+
+/**
+ * Project: Falcron
+ * Author: itsfizys (Aegis)
+ * Organization: AeroX Development
+ * GitHub: https://github.com/AeroXDevs
+ * License: Custom
+ *
+ * © 2026 AeroX Development. All rights reserved.
+ */
